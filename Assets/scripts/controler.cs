0b8f87445f49f499c94d94dd08fd3c2216df3832
Margarita Martinez <est.margarita.mart@unimilitar.edu.co>
2022-03-13 22:21:26 -0500

    lluvia unity proyecto

---
+++ Assets/scripts/controler.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class controler : MonoBehaviour
+{
+    public GameObject gotita;
+    public int gotitaC;
+    public float WINDSPEED;
+    private Slider windSlider, gotitasSlider;
+    public Text datoWind, datoGotitas;
+    public Button iniciar, modificar, salir;
+    private bool start = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Generador", 0.5f, 0.5f);
+
+        windSlider = GameObject.Find("SliderViento").GetComponent<Slider>();
+        gotitasSlider = GameObject.Find("SliderGotas").GetComponent<Slider>();
+        datoWind = GameObject.Find("TextDatoViento").GetComponent<Text>();
+        datoGotitas = GameObject.Find("TextDatoGotas").GetComponent<Text>();
+
+        iniciar.onClick.AddListener(IniciarPro);
+        modificar.onClick.AddListener(Modifi);
+        salir.onClick.AddListener(Exit);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void IniciarPro()
+    {
+        start = true;
+    }
+    void Modifi()
+    {
+        start = false;
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void TomaDatos()
+    {
+        WINDSPEED = windSlider.value;
+        gotitaC = (int)gotitasSlider.value;
+        datoWind.text = WINDSPEED.ToString();
+        datoGotitas.text = gotitaC.ToString();
+    }
+
+    void Generador()
+    {
+        if (start)
+        {
+            for (int i = 0; i < gotitaC; i++)
+            {
+                Instantiate(gotita, new Vector3(Random.Range(-15, 15), Random.Range(8, 14), Random.Range(-4, 4)), Quaternion.identity);
+            }
+        }
+        else
+        {
+            TomaDatos();
+        }
+    }
+
+}
---
+++ Assets/scripts/lluvia.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lluvia : MonoBehaviour
+{
+    public float fMass, fSpeed, fRadius;
+    public Vector3 vPosition, vVelocity, vForces, vGravity;
+    private float GRAVITY = -9.8f;
+    public float AIRDENSITY = 1.23f, DRAGCOE = 0.6f, WINDSPEED;
+    public controler control;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fMass = 1.0f;
+        vPosition = this.transform.position;
+        vVelocity = Vector3.zero;
+        fSpeed = 0;
+        vForces = Vector3.zero;
+        fRadius = 0.1f;
+        vGravity = Vector3.zero;
+        vGravity.y = fMass * GRAVITY;
+
+        control = FindObjectOfType<controler>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalForces();
+        UpdateBody(Time.deltaTime);
+        WINDSPEED = control.WINDSPEED;
+    }
+    void CalForces()
+    {
+        //reset forces
+        vForces.x = 0.0f;
+        vForces.y = 0.0f;
+
+        //aggregate forces
+        vForces += vGravity;
+
+        //still air drag
+        Vector3 vDrag = Vector3.zero;
+        float fDrag;
+
+        vDrag -= vVelocity;
+        vDrag = vDrag.normalized;
+        fDrag = 0.5f * AIRDENSITY * fSpeed * fSpeed * (Mathf.PI * fRadius * fRadius) * DRAGCOE;
+
+        vDrag *= fDrag;
+
+        vForces += vDrag;
+
+        //windUwU
+        Vector3 vWind = Vector3.zero;
+        vWind.x = 0.5f * AIRDENSITY * WINDSPEED * WINDSPEED * (Mathf.PI * fRadius * fRadius) * DRAGCOE;
+        vForces += vWind;
+    }
+    void UpdateBody(float dt)
+    {
+        Vector3 a, dv, ds;
+
+        //integrate equation of motion
+        a = vForces / fMass;
+
+        dv = a * dt;
+        vVelocity += dv;
+
+        ds = vVelocity * dt;
+        vPosition += ds;
+
+        //misc. calculation
+        fSpeed = vVelocity.magnitude;
+        this.transform.position = vPosition;
+    }
+}
